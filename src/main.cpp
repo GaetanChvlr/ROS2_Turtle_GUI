18e2167ce1c86c1a941717ce6d4b3f91b6fb4e20
GaetanChvlr <gaetanchevalier28@gmail.com>
2024-03-30 18:48:18 +0100

    first window

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//#include <rclcpp/rclcpp.h>
+#include <QApplication>
+#include "qt/MainWindow.hpp"
+
+int main(int argc, char *argv[]) {
+    //rclcpp::init(argc,argv);        // ROS
+    QApplication app(argc, argv);   // QT
+
+    // Turtlesim node en arriere plan
+    //system("ros2 run turtlesim turtlesim_node &");
+    
+    MainWindow mainWin;
+    mainWin.show();
+    int res = app.exec();
+    
+    //system("killall -q turtlesim_node");
+    //rclcpp::shutdown();
+    return res;
+}
